9cbaded47c2ee715258cd231914dc96a576b0649
Will Fisher <wfisher@chef.io>
2019-01-30 11:02:09 -0800

    Update to use grese instead of gatherlogs
    Signed-off-by: Will Fisher <wfisher@chef.io>

--- recipes/default.rb
+++ recipes/default.rb
@@ -8,17 +8,37 @@
 
 hab_sup 'default'
 
+ruby_block 'wait-for-sup-default-startup' do
+  block do
+    raise unless system('hab sup status')
+  end
+  retries 30
+  retry_delay 1
+end
+
 gatherlogs = chef_vault_item("secrets", "gatherlogs")
-hab_user_toml 'gatherlogs' do
+hab_user_toml 'grese' do
   config({
     server: gatherlogs.to_hash
   })
 end
 
-hab_package 'will/gatherlogs'
+hab_package 'will/grese'
 
-hab_service 'will/gatherlogs' do
+hab_service 'will/grese' do
   strategy 'at-once'
   topology 'standalone'
   channel node['gatherlogs']['channel']
+end
+
+# we need to sleep to let the nginx service have enough time to
+# startup properly before we can unload it.
+ruby_block 'wait-for-grese-startup' do
+  block do
+    raise unless system('hab svc status will/grese')
+  end
+  retries 30
+  retry_delay 1
+  action :nothing
+  subscribes :run, 'hab_service[will/grese]', :immediately
 end
